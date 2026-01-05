78771c6066c5f487334784928bb9049efc831e37
Shruti Bachal <144803678+ShrutiBachal@users.noreply.github.com>
2026-01-05 09:38:33 +0530

    Refactor MsgType enum and update Message constructor
    Updated MsgType enum to use string values, modified Message class constructor to include accepted_id as an optional parameter and formatted the output to produce unambiguous logs.

--- message.py
+++ message.py
@@ -2,16 +2,24 @@
 from enum import Enum
 
 class MsgType(Enum):
-    PREPARE = 1
-    PROMISE = 2
-    ACCEPT = 3
-    ACCEPTED = 4
-    LEARN = 5
+    PREPARE = "PREPARE"
+    PROMISE = "PROMISE"
+    ACCEPT = "ACCEPT"
+    ACCEPTED = "ACCEPTED"
+    LEARN = "LEARN"
 
 class Message:
-    def __init__(self, msg_type, src, dst, value, proposal_id=None):  # Order of args matter non-default should be before default
+    def __init__(self, msg_type, src, dst, value = None, proposal_id = None,accepted_id = None):  # Order of args matter non-default should be before default
+    # Making it None allows the value to be optional i.e., no compulsion to always declare it
         self.msg_type = msg_type
         self.src = src
         self.dst = dst
         self.proposal_id = proposal_id
         self.value = value
+        self.accepted_id = accepted_id
+
+    def __repr__(self):
+        return (f"{self.msg_type} "
+                f"from {self.src} "
+                f"pid={self.proposal_id} "
+                f"value={self.value}")
