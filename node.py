c92cc94ae820e35b044053ab00119de87869fea5
Shruti Bachal <144803678+ShrutiBachal@users.noreply.github.com>
2026-01-05 09:21:15 +0530

    Updated node.py with Paxos logic

--- node.py
+++ node.py
@@ -1,11 +1,26 @@
 %%writefile core/node.py
 import asyncio
+from core.message import MsgType, Message
+from paxos.proposer import Proposer
+from paxos.acceptor import Acceptor
 
 class Node:
-    def __init__(self, node_id,network):
+    def __init__(self, node_id,network,peers):
         self.node_id = node_id
         self.network = network
         self.inbox = asyncio.Queue()
+        self.peers = peers
+
+        # Paxos state (Acceptor)
+        self.promised_id = None
+        self.accepted_id = None
+        self.accepted_value = None
+        self.acceptor = Acceptor(self)
+
+        # Paxos state (Proposer)
+        self.proposal_seq = 0
+        self.promises = {}
+        self.proposer = Proposer(self)
 
     async def run(self):
         while True:
@@ -13,4 +28,16 @@ class Node:
             await self.handle(msg)
 
     async def handle(self, msg):
-        print(f"Node {self.node_id} received {msg.value}")
+      if msg.msg_type == MsgType.PREPARE:
+          await self.acceptor.on_prepare(msg)
+
+      elif msg.msg_type == MsgType.PROMISE:
+          await self.proposer.on_promise(msg)
+
+      elif msg.msg_type == MsgType.ACCEPT:
+          await self.acceptor.on_accept(msg)
+
+      #print(f"Node {self.node_id} received {msg.value}")
+
+      # elif msg.msg_type == MsgType.ACCEPTED:
+      #     await self.on_accepted(msg)
