09d7340a7391846768d1093b0771fca50a6fbe0f
Shruti Bachal <144803678+ShrutiBachal@users.noreply.github.com>
2026-01-10 12:18:29 +0530

    Added visualizer support to Network class
    Added visualizer parameter to the Network class for tracking message flow.

--- network.py
+++ network.py
@@ -3,17 +3,26 @@ import asyncio
 import random
 
 class Network:
-    def __init__(self, min_delay=0.05, max_delay=0.2, loss_rate=0.3):
+    def __init__(self, min_delay=0.05, max_delay=0.2, loss_rate=0.3,visualizer = None):
         self.nodes = {}
         self.min_delay = min_delay
         self.max_delay = max_delay
         self.loss_rate = loss_rate
+        self.visualizer = visualizer
 
     def register(self, node):
         """Register a node with the network"""
         self.nodes[node.node_id] = node
 
     async def send(self, message):
+        if self.visualizer:
+            self.visualizer.record(
+                message.src,
+                message.dst,
+                message.msg_type.value
+            )
+            self.visualizer.draw()
+            
         """Send a message with simulated delay and loss"""
         # Simulate message loss
         if random.random() < self.loss_rate:
