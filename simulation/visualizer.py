3a9761c9f060eacb5f0f2f3af7d118f5d2c32ab5
Shruti Bachal <144803678+ShrutiBachal@users.noreply.github.com>
2026-01-25 00:54:46 +0530

    Updated visualizer to distinguish between current and previous rounds and mentioned consensus time in output

--- simulation/visualizer.py
+++ simulation/visualizer.py
@@ -4,10 +4,13 @@ import matplotlib.pyplot as plt
 from collections import deque
 
 class PaxosVisualizer:
-  def __init__(self):
+  def __init__(self,network):
+    self.network = network
     self.G = nx.DiGraph()
-    self.events = deque(maxlen=20)  # recent messages
+    self.events = deque(maxlen=20)            # recent messages
     self.pos = nx.spring_layout(self.G)
+    self.active_round = None
+    self.p_id = None
 
     self.colors = {
         "PREPARE": "blue",
@@ -20,9 +23,18 @@ class PaxosVisualizer:
         for n in node_ids:
             self.G.add_node(n)
         self.pos = nx.circular_layout(self.G)
+    
+  def set_active_round(self, proposer_id, round_id):          # used for distinct round visualiztion
+    self.active_round = (proposer_id, round_id)
 
-  def record(self, src, dst, msg_type):
-        self.events.append((src, dst, msg_type))
+  def record(self, src, dst, msg_type, proposer_id, round_id):
+    self.events.append({
+        "src": src,
+        "dst": dst,
+        "type": msg_type,
+        "proposer": proposer_id,
+        "round": round_id
+    })
 
   def learned(self, node_id, value):
       plt.text(
@@ -45,6 +57,18 @@ class PaxosVisualizer:
     plt.clf()
     ax = plt.gca()
 
+    if (self.network.consensus_reached):    
+        t = (self.network.end_time - self.network.start_time)    # Displaying total time taken for single consensus 
+        if t is not None:
+          plt.text(
+            0.5, -0.15,
+            f"Consensus Time: {t:.4f} seconds",
+            fontsize=11,
+            color="black",
+            ha="center",
+            transform=plt.gca().transAxes
+          )
+          
     # draw nodes
     nx.draw_networkx_nodes(
         self.G, self.pos,
@@ -53,9 +77,25 @@ class PaxosVisualizer:
     )
     nx.draw_networkx_labels(self.G, self.pos,font_size=10)
 
-    for src, dst, msg_type in self.events:
-        color = self.colors.get(msg_type, "black")
+    for event in self.events:
+        src = event["src"]
+        dst = event["dst"]
+        msg_type = event["type"]
+        proposer = event["proposer"]
+        round_id = event["round"]
 
+        is_active = (proposer, round_id) == self.active_round
+        base_color = self.colors.get(msg_type, "black")
+
+        if is_active:
+            color = base_color
+            width = 2.5
+            alpha = 1.0
+        else:
+            color = "lightgrey"              # color fading if not the current round
+            width = 1.0
+            alpha = 0.4
+          
         # draw edges
         nx.draw_networkx_edges(
             self.G,
@@ -64,16 +104,18 @@ class PaxosVisualizer:
             arrowstyle="-|>",
             arrowsize=20,
             edgelist=[(src, dst)],
-            edge_color = color,   # your color list
-            width=2,
+            edge_color = color,               # your color list
+            width=width,
+            alpha=alpha,
             connectionstyle="arc3,rad=0.1"
         )
 
         # label
+        label = f"P{proposer}-R{round_id}"    # labeling the path with Proposer_id and round_id instead of Message type
         nx.draw_networkx_edge_labels(
             self.G,
             self.pos,
-            edge_labels={(src, dst): msg_type},
+            edge_labels={(src, dst): label},
             font_color=color,
             font_size=9,
             label_pos=0.5
@@ -81,4 +123,4 @@ class PaxosVisualizer:
 
     plt.title("Paxos Message Flow")
     plt.axis("off")
-    plt.pause(0.004)
+    plt.pause(0.005)
