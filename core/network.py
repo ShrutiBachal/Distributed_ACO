225114e86076668236359df858a341fa289bc268
Shruti Bachal <144803678+ShrutiBachal@users.noreply.github.com>
2026-01-24 19:25:50 +0530

    Implemented consensus time tracking and introduced proposer_id, round_id in visualizer record.
    Added consensus time calculation and updated visualizer record with proposer and it's respective round_id to easily visualize the active round, preventing the ambiguity with previous round traces.

--- core/network.py
+++ core/network.py
@@ -9,17 +9,30 @@ class Network:
         self.max_delay = max_delay
         self.loss_rate = loss_rate
         self.visualizer = visualizer
+        self.consensus_reached = False
+        self.start_time = 0
+        self.end_time = 0
 
     def register(self, node):
         """Register a node with the network"""
         self.nodes[node.node_id] = node
 
+    def consensus_time(self):                        # calculates the time required to reach consensus
+      if self.start_time and self.end_time:
+          return self.end_time - self.start_time
+      return None
+
     async def send(self, message):
+        if self.start_time is None:
+            self.start_time = time.monotonic()
+            
         if self.visualizer:
-            self.visualizer.record(
+            self.visualizer.record(         
                 message.src,
                 message.dst,
-                message.msg_type.value
+                message.msg_type.value,
+                message.proposal_id[1],     # proposer_id
+                message.proposal_id[0],     # round_id (1,3) (2-> round, 3->node_id)
             )
             self.visualizer.draw()
             
