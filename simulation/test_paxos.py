c61f580551f32084b9d4cfaeac70aa767d9be74a
Shruti Bachal <144803678+ShrutiBachal@users.noreply.github.com>
2026-01-25 01:25:30 +0530

    Adjusted network delay and updated proposer node

--- simulation/test_paxos.py
+++ simulation/test_paxos.py
@@ -8,17 +8,12 @@ from core.network import Network
 from core.node import Node
 
 async def main():
-    net = Network(min_delay=0.1, max_delay=0.5)
+    net = Network(min_delay=0.05, max_delay=0.2)
 
     # create 3 nodes (classic Paxos)
     node_ids = [1, 5, 2]
     nodes = {}
-
-    visualizer = PaxosVisualizer()
-    visualizer.register_nodes(node_ids)
-
-    net.visualizer = visualizer
-
+    
     for nid in node_ids:
         nodes[nid] = Node(
             node_id=nid,
@@ -26,6 +21,10 @@ async def main():
             peers=node_ids
         )
         net.register(nodes[nid])
+        
+    visualizer = PaxosVisualizer()
+    visualizer.register_nodes(node_ids)
+    net.visualizer = visualizer
 
     # start nodes
     for node in nodes.values():
@@ -33,7 +32,7 @@ async def main():
 
     # choose node 1 as proposer
     await asyncio.sleep(1)   # let event loop settle
-    await nodes[1].proposer.propose("VALUE_X")  # node can access proposer class due to Proposer(self) passed to it's self.proposer
+    asyncio.create_task(nodes[2].proposer.propose("VALUE_2")) # node can access proposer class due to Proposer(self) passed to it's self.proposer
 
     # let Paxos finish
     await asyncio.sleep(5)
